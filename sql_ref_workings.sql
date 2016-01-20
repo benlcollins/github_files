d10118c86ece63a9a48ee2fcb927fc6ef4d408e5
Ben Collins <benlcollins@gmail.com>
2016-01-20 15:46:38 -0500

    Update sql_ref_workings.sql

--- sql_ref_workings.sql
+++ sql_ref_workings.sql
@@ -14,31 +14,31 @@
 DROP TABLE IF EXISTS ref_destination_data;
 
 CREATE TABLE ref_destination_data (
-	id 													SERIAL NOT NULL,
-	Textbox87										text,
-	Textbox82										text,
-	nat_definition4							text,
-	region_name_3								text,
-	textbox37										text,
-	Category3										text,
-	textbox39										text,
+	id 					SERIAL NOT NULL,
+	Textbox87				text,
+	Textbox82				text,
+	nat_definition4				text,
+	region_name_3				text,
+	textbox37				text,
+	Category3				text,
+	textbox39				text,
 	Assur_DestinationCity1			text,
-	Cases3											text,
-	Cases4											text
+	Cases3					text,
+	Cases4					text
 );
 
 DROP TABLE IF EXISTS ref_religion_data;
 
 CREATE TABLE ref_religion_data (
-	id 													SERIAL NOT NULL,
-	Textbox126									text,
-	Textbox127									text,
-	nat_definition3							text,
-	region_name_2								text,
-	textbox36										text,
-	relig_definition1						text,
-	Cases												text,
-	Cases2											text
+	id 					SERIAL NOT NULL,
+	Textbox126				text,
+	Textbox127				text,
+	nat_definition3				text,
+	region_name_2				text,
+	textbox36				text,
+	relig_definition1			text,
+	Cases					text,
+	Cases2					text
 );
 
 ----------------------------------------------------------------------------------
@@ -114,9 +114,9 @@ ALTER TABLE ref_religion_data RENAME cases2 TO total;
 -- summarize by origin, from highest to lowest
 ----------------------------------------------------------------------------------
 SELECT 	origin
-				, sum(num_unique) AS number_refugees
-				, total
-				, round((sum(num_unique)*1.0/total)*100,1) AS refugee_ratio
+	, sum(num_unique) AS number_refugees
+	, total
+	, round((sum(num_unique)*1.0/total)*100,1) AS refugee_ratio
 FROM ref_destination_data
 GROUP BY origin, total
 ORDER BY 2 DESC;
@@ -126,9 +126,9 @@ ORDER BY 2 DESC;
 ----------------------------------------------------------------------------------
 WITH ref_stats AS (
 	SELECT 	origin
-					, sum(num_unique) AS number_refugees
-					, total
-					, round((sum(num_unique)*1.0/total)*100,1) AS refugee_ratio
+		, sum(num_unique) AS number_refugees
+		, total
+		, round((sum(num_unique)*1.0/total)*100,1) AS refugee_ratio
 	FROM ref_destination_data
 	GROUP BY origin, total
 	ORDER BY 2 DESC
@@ -144,10 +144,10 @@ LIMIT 20;
 -- summarize data by state
 ----------------------------------------------------------------------------------
 SELECT 	state
-				, num_state AS state_refs
-				, sum(num_unique) AS calculation_state_refs	-- this is a check that sum of uniques agrees
-				, total AS total_refugees
-				, round((num_state*1.0/total)*100,2) AS refugee_ratio
+	, num_state AS state_refs
+	, sum(num_unique) AS calculation_state_refs	-- this is a check that sum of uniques agrees
+	, total AS total_refugees
+	, round((num_state*1.0/total)*100,2) AS refugee_ratio
 FROM ref_destination_data
 GROUP BY state, num_state, total
 ORDER BY 1,2 ASC;
@@ -156,9 +156,9 @@ ORDER BY 1,2 ASC;
 -- sort by state, highest to lowest numbers
 ----------------------------------------------------------------------------------
 SELECT 	state
-				, num_state AS state_refs
-				, total AS total_refugees
-				, round((num_state*1.0/total)*100,2) AS refugee_ratio
+	, num_state AS state_refs
+	, total AS total_refugees
+	, round((num_state*1.0/total)*100,2) AS refugee_ratio
 FROM ref_destination_data
 GROUP BY state, num_state, total
 ORDER BY 2 DESC;
@@ -167,8 +167,8 @@ ORDER BY 2 DESC;
 -- summary statistics by Country of Origin
 ----------------------------------------------------------------------------------
 SELECT 	sum(number_refugees) AS total_refs
-				, round(avg(number_refugees)) AS average_per_country
-				, round(stddev(number_refugees)) AS std_deviation
+	, round(avg(number_refugees)) AS average_per_country
+	, round(stddev(number_refugees)) AS std_deviation
 FROM (
 	SELECT	origin, sum(num_unique) AS number_refugees
 	FROM ref_destination_data
@@ -181,7 +181,7 @@ FROM (
 -- summary statistics by State
 ----------------------------------------------------------------------------------
 SELECT 	round(avg(number_refugees)) as average_by_state
-				, round(stddev(number_refugees)) as std_deviation_by_state
+	, round(stddev(number_refugees)) as std_deviation_by_state
 FROM (
 	SELECT	state, sum(num_unique) AS number_refugees
 	FROM ref_destination_data
@@ -194,9 +194,9 @@ FROM (
 -- summary by religion, high to low
 ----------------------------------------------------------------------------------
 SELECT 	religion
-				, sum(num_unique) as num_religion
-				, total
-				, round((sum(num_unique)*1.0/total)*100,1) AS refugee_ratio
+	, sum(num_unique) as num_religion
+	, total
+	, round((sum(num_unique)*1.0/total)*100,1) AS refugee_ratio
 FROM ref_religion_data
 GROUP BY religion, total
 ORDER BY 2 DESC;
